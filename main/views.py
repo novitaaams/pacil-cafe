ddc77741fee24628951fe9b81db981d0a79425ca
novitaaams <novitaaams@gmail.com>
2023-09-19 11:42:37 +0700

    test

--- main/views.py
+++ main/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from main.forms import ProductForm
+from django.urls import reverse
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
-        'nama_aplikasi': 'Pacil Cafe',
-        'nama': 'Novita Mulia Sari',
-        'kelas': 'PBP A'
+        'name': 'Novita Mulia Sari', # Nama kamu
+        'class': 'PBP A', # Kelas PBP kamus
+        'products': products
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
